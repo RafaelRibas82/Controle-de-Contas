d1706865ee23608d9998ab781dffd6537424e01d
rafae <rafae@DESKTOP-7DADMO6>
2023-09-06 20:25:32 -0300

    Adicionar arquivos de projeto.

---
+++ Controle de Contas/Conta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controle_de_Contas
+{
+    internal class Conta
+    {
+        //private int _numero;
+        private decimal _saldo;
+        public int Numero { get; set; }
+    }
+}
